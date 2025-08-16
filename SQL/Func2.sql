6a7d8398ca3c455517ae41812e39a446c9a96025
Nishitha Deverakonda <165659051+nishcode04@users.noreply.github.com>
2025-08-16 09:25:35 +0530

    Update Func2.sql

--- SQL/Func2.sql
+++ SQL/Func2.sql
@@ -7,15 +7,10 @@ BEGIN
     DECLARE totalSpending DECIMAL(10,2);
 
     -- Calculate total spending by the customer across all orders
-SELECT 
-    SUM(p.Amount)
-INTO totalSpending FROM
-    Payments p
-        JOIN
-    Orders o ON p.OrderID = o.OrderID
-WHERE
-    o.CustomerID = customerID;
-
+    SELECT SUM(p.Amount) INTO totalSpending 
+    FROM Payments p JOIN Orders o
+    ON p.OrderID = o.OrderID
+    WHERE o.CustomerID = customerID;
     RETURN totalSpending;
 END $$
 
